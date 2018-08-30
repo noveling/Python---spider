f9dfcd0f7adfe3af9cb734a0c54659082eed6c97
noveling <2823502386@qq.com>
2018-08-30 12:02:58 +0800

    豆瓣爬虫

---
+++ bookspider/bookspider/settings.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for bookspider project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'bookspider'
+
+SPIDER_MODULES = ['bookspider.spiders']
+NEWSPIDER_MODULE = 'bookspider.spiders'
+
+IPPOOL = [
+    {"ipaddr":"183.129.207.73:14823"},
+    {"ipaddr":"123.163.80.150:29826"},
+    {"ipaddr":"36.99.207.229:808"},
+    {"ipaddr":"49.85.1.20:21809"},
+    {"ipaddr":"123.163.162.123:38986"},
+    {"ipaddr":"125.109.198.108:20846"},
+    {"ipaddr":"118.31.223.194:3128"},
+    {"ipaddr":"218.60.8.99:3129"},
+    {"ipaddr":"106.56.102.171:8070"},
+    {"ipaddr":"118.190.210.227:3128"},
+    {"ipaddr":"144.123.69.94:21919"},
+]
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'bookspider.middlewares.BookspiderSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+   # 'bookspider.middlewares.BookspiderDownloaderMiddleware': 543,
+   'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 543,
+   'bookspider.middlewares.BookspiderDownloaderMiddleware': 125,
+}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   'bookspider.pipelines.BookspiderPipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
---
+++ bookspider/bookspider/spiders/bookcrawler.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from bookspider.items import BookspiderItem
+import re
+
+
+class BookcrawlerSpider(CrawlSpider):
+    name = "bookcrawler"
+    allowed_domains = ['book.douban.com']
+    start_urls = ['http://book.douban.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'^https://book.douban.com/tag/.+'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        i = BookspiderItem()
+        info = []
+        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        #i['name'] = response.xpath('//div[@id="name"]').extract()
+        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        nameinfo = response.xpath("//ul/li/div[@class='info']/h2[@class]/a/text()").extract()
+        for name in nameinfo:
+            final = re.sub(r"[' ','\n']","",name,flags=re.S)
+            if final:
+                info.append(final)
+        i["bookurl"] = response.xpath("//ul/li/div[@class='info']/h2[@class]/a/@href").extract()
+        i["bookname"] = info
+
+        yield i
