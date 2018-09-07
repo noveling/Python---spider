2be67eeb68438195ff846313289381a52292e1a4
noveling <2823502386@qq.com>
2018-09-07 11:17:20 +0800

    ajax爬豆瓣电影

--- ajaxdouban/ajaxdouban/pipelines.py
+++ ajaxdouban/ajaxdouban/pipelines.py
@@ -9,7 +9,7 @@ import codecs
 class AjaxdoubanPipeline(object):
     def __init__(self):
         self.fp = codecs.open('movielist.html','w',encoding='utf-8')
-        self.fp.write("<html><head><meta charset='utf-8'/><title>豆瓣电影清单</title></head><table><th>电影名</th><th>评分</th><th>电影链接</th><th>图片</th>")
+        self.fp.write("<html><head><meta charset='utf-8'/><title>豆瓣电影清单</title><style>table{font-size:20px}</style></head><table><th>电影名</th><th>评分</th><th>电影链接</th><th>图片</th>")
 
     def process_item(self, item, spider):
         self.fp.write("<tr><td>"+item['moviename']+"</td><td>"+item['score']+"</td><td>"+item['movieurl']+"</td><td><img src='"+item['movieimg']+"'></td></tr>")
--- ajaxdouban/ajaxdouban/spiders/ajaxspider.py
+++ ajaxdouban/ajaxdouban/spiders/ajaxspider.py
@@ -2,19 +2,24 @@
 import scrapy
 from ajaxdouban.items import AjaxdoubanItem
 import json
+from scrapy.conf import settings
+
 
 class AjaxspiderSpider(scrapy.Spider):
+    # 'https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start='
+    # 'https://movie.douban.com/j/new_search_subjects?sort=T&range=0,10&tags=&start=100'
     name = 'ajaxspider'
     allowed_domains = ['douban.com']
     page_offset = 0
     flag = 1
+    cook = settings['COOKIE']
     baseurl = 'https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start='
-    start_urls = [''.join([baseurl,str(page_offset)])]
+    start_urls = [''.join([baseurl, str(page_offset)])]
 
     def parse(self, response):
         item = AjaxdoubanItem()
         data = json.loads(response.text)["subjects"]
-        # print(data)
+        # data = json.loads(response.text)["data"]
         if data:
             for each in data:
                 item["moviename"] = each["title"]
@@ -26,4 +31,5 @@ class AjaxspiderSpider(scrapy.Spider):
             self.flag = 0
         self.page_offset += 20
         if self.flag:
-            yield scrapy.Request(''.join([self.baseurl,str(self.page_offset)]),callback=self.parse)
+            yield scrapy.Request(''.join([self.baseurl, str(self.page_offset)]), cookies=self.cook, callback=self.parse)
+            # print(response.request.headers)
