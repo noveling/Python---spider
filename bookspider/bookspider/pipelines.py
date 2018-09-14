90e41ddee95c1e63f65e1641b8f2683c0fc041f9
noveling <2823502386@qq.com>
2018-09-14 14:37:11 +0800

    Python爬虫

--- bookspider/bookspider/pipelines.py
+++ bookspider/bookspider/pipelines.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#__author__ = "noveling"
 
 # Define your item pipelines here
 #
--- bookspider/bookspider/spiders/bookcrawler.py
+++ bookspider/bookspider/spiders/bookcrawler.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#__author__ = "noveling"
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
