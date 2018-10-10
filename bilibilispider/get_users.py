4259474f68d011c28f8e442560729440ab9caa43
noveling <2823502386@qq.com>
2018-10-10 10:40:16 +0800

    爬b站

--- bilibilispider/get_users.py
+++ bilibilispider/get_users.py
@@ -5,6 +5,7 @@ import json
 import random
 from create_ippool import get_html
 import os
+from urllib.request import urlretrieve
 
 ip_pool = [{"http":"http://"+i} for i in get_html(20)]
 print(ip_pool)
@@ -76,7 +77,14 @@ def getsource(url):
             rel_info = json.loads(jscontent, encoding="utf-8")
             print(rel_info)
     finally:
-        if rel_info["data"] != '服务器遇到了一些问题' and rel_info:
+        if rel_info["data"] != '服务器遇到了一些问题' and rel_info["data"]["face"]:
+            print("downloading......" + rel_info["data"]["face"])
+            if not os.path.exists("./image"):
+                os.mkdir("./image")
+
+            if not os.path.exists('./image/'+ rel_info["data"]["face"].split('/')[-1]):
+                print("正在下载...")
+                urlretrieve(rel_info["data"]["face"],'./image/'+ rel_info["data"]["face"].split('/')[-1])
             with open("./bilibili.txt","a") as fp:
                     json.dump(rel_info,fp,ensure_ascii=False)
                     fp.write("\n")
@@ -86,7 +94,7 @@ def getsource(url):
 if __name__ == "__main__":
     if os.path.exists("./bilibili.txt"):
         os.remove("./bilibili.txt")
-    pool = ThreadPool(10)
+    pool = ThreadPool(5)
     try:
         results = pool.map(getsource, urls)
     except Exception as e:
